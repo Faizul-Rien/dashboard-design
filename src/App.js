37007e8e63d867754a7dc2d93fa471d6994ad86a
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-09-16 20:38:11 +0600

    successfully install ant design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
      <h2>Hello Dashboard</h2>
     </div>
   );
---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import { AliwangwangOutlined } from '@ant-design/icons';
+import React from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className='nav'><AliwangwangOutlined/>LOOGO</h2>
+        </div>
+    );
+};
+
+export default Navbar;
