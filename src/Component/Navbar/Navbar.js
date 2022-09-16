782e09618cfd55936c66e1a710553849b9f7fe8b
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-09-17 00:08:21 +0600

    create navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,11 +1,25 @@
-import { AliwangwangOutlined } from '@ant-design/icons';
+import { DownOutlined, SearchOutlined } from '@ant-design/icons';
+import { Button,Input} from 'antd';
 import React from 'react';
+import bell from '../../Images/bell.svg';
+import man from '../../Images/man.png';
 import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <h2 className='nav'><AliwangwangOutlined/>LOOGO</h2>
+        <div className='navbar'>
+             <h2 className='nav'><Button className='logo' shape="circle">L</Button>LOOGO</h2>
+             <div className='search-input'>
+             <Input className='search' placeholder="Search " icon= {<SearchOutlined />} /> 
+
+             </div>
+             <div className='notification'>
+                <img src={bell} alt="" />
+                <img src={man} alt="" />
+                <h2>Faizul</h2>
+                <p><DownOutlined /></p>
+             </div>
+             
         </div>
     );
 };
