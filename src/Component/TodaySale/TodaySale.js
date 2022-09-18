457463a8fcdd24949429849b9f006d5672d15c90
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-09-19 01:32:31 +0600

    add sales unit

---
+++ src/Component/TodaySale/TodaySale.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './TodaySale.css';
+import { Pie } from '@ant-design/plots';
+import { Button } from 'antd';
+
+
+const data = [
+    {
+      type: 'TV',
+      value: 50,
+    },
+    {
+      type: 'Laptop',
+      value: 25,
+    },
+    {
+      type: 'Other',
+      value: 25,
+    }
+  ];
+  const config = {
+    appendPadding: 10,
+    data,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: 'inner',
+      offset: '-50%',
+    //   content: '{value}',
+      style: {
+        textAlign: 'center',
+        fontSize: 12,
+      },
+    },
+    interactions: [
+      {
+        type: 'element-selected',
+      },
+      {
+        type: 'element-active',
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: 'pre-wrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        },
+        // content: 'AntV\nG2Plot',
+      },
+    },
+  };
+
+const TodaySale = () => {
+    return (
+        <div>
+            
+             <div className='today-sale'>
+             <div>
+                <h2 className='s-title'>Sales Chart</h2>
+                <p className='sale-t'>Todays Sale</p>
+                <h2 className='s-title'>156</h2>
+                
+            </div>
+             <Pie style={{height:"150px"}} className='pie-chart' {...config} />
+             <div>
+             <Button className='btn' type="link">
+        See details
+      </Button>
+             </div>
+             </div>
+        </div>
+    );
+};
+
+export default TodaySale;
--- src/Component/TotalSale/TotalSale.js
+++ src/Component/TotalSale/TotalSale.js
@@ -2,38 +2,91 @@ import React from 'react';
 import './TotalSale.css';
 import { DatePicker, Space } from 'antd';
 import moment from 'moment';
+import { Line } from '@ant-design/plots';
+import TodaySale from '../TodaySale/TodaySale';
+
+const data = [
+    {
+      "item": 0,
+      "scales": "1JT"
+    },
+    {
+      "item": 7,
+      "scales": "2JT"
+    },
+    {
+      "item": 14,
+      "scales": "3JT"
+    },
+    {
+      "item": 21,
+    //   "scales": 4
+    },
+    {
+      "item": 28,
+    //   "scales": 5
+    }
+  
+  ]
+  const config = {
+    data,
+    padding: 'auto',
+    xField: 'item',
+    yField: 'scales',
+    xAxis: {
+    //   type: 'timeCat',
+      tickCount: 5,
+    },
+    smooth: true,
+}
 
 
 const dateFormat = 'YYYY-MM-DD';
 const TotalSale = () => {
     return (
-        <div className='sale-container'>
-           <div className='date-picker'>
-           <h2>Total Sale</h2>
-            <Space direction="vertical" size={12}>
-    <DatePicker defaultValue={moment('2022-08-08', dateFormat)}  />
-    </Space>
-           </div>
-
-           <div>
-                <p className='sale-item'>TV</p>
-                <h2 className='sales-quantity'>600.000</h2>
-            
-           </div>
-
-           <div>
-                <p className='sale-item'>Laptop</p>
-                <h2 className='sales-quantity'>1.200.000</h2>
-           </div>
-
-           <div>
-                <p className='sale-item'>Others</p>
-                <h2 className='sales-quantity'>210.287</h2>
-           </div>
+       <div className='container'>
+         <div className='sale-container'>
+       <div className='date-picker'>
+       <h2>Total Sale</h2>
+        <Space direction="vertical" size={12}>
+<DatePicker defaultValue={moment('2022-08-08', dateFormat)}  />
+</Space>
+       </div>
 
 
+    <div className='chart-details'>
+    <div>
+      <div>
+            <p className='sale-item'>TV</p>
+            <h2 className='sales-quantity'>600.000</h2>
+        
+       </div>
 
-        </div>
+       <div>
+            <p className='sale-item'>Laptop</p>
+            <h2 className='sales-quantity'>1.200.000</h2>
+       </div>
+
+       <div>
+            <p className='sale-item'>Others</p>
+            <h2 className='sales-quantity'>210.287</h2>
+       </div>
+      </div>
+
+
+<div className='chart-info'>
+      
+<Line {...config} />
+</div>
+    </div>
+
+
+
+    </div>
+    <div>
+        <TodaySale></TodaySale>
+    </div>
+    </div>
     );
 };
 
